e60c3e306335b42fd1a70d2be7c032dd883af1f4
Andrewthetate <kevin.meier@ws-five.ch>
2025-12-01 10:48:26 +0100

    Updated trace

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4fbcf410d5b24c87"; }
+/******/ 		__webpack_require__.h = function() { return "8e69ced61f8b15f1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
